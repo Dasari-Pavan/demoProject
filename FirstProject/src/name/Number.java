1ec49755bdc9b13b7af82871007bdae4c984fafc
LENOVO <LENOVO@LENOVO-PC>
2022-11-24 13:47:30 +0530

    second commit

--- FirstProject/src/name/Number.java
+++ FirstProject/src/name/Number.java
@@ -3,6 +3,13 @@ package name;
 public class Number {
 	public static void main(String[] args) {
 		System.out.println("First Demo");
+		
+		Number num = new Number();
+		
+		
 	}
-
+	void New()
+	{
+		System.out.println("hello");
+	}
 }
